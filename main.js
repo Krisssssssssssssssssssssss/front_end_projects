11db59f0e5e0c39d4329d8a822ffd1d894404eb9
Kristijan Jakimov <kiko.jakimov@gmail.com>
2023-01-22 14:32:02 +0100

    JS template added

---
+++ main.js
@@ -0,0 +1,47 @@
+document.querySelector("#filter-coding").addEventListener("click", filterCoding);
+        document.querySelector("#filter-design").addEventListener("click", filterDesign);
+        document.querySelector("#filter-marketing").addEventListener("click", filterMarketing);
+        document.querySelector("#filter-all").addEventListener("click", showAllCards);
+
+        function filterCoding() {
+            hideAllCards();
+
+            var codingCards = document.querySelectorAll(".coding");
+            codingCards.forEach(codingCard => {
+                codingCard.style.display = "inline-block";
+            });
+        }
+
+        function filterDesign() {
+            hideAllCards();
+
+            var designCards = document.querySelectorAll(".design");
+            designCards.forEach(designCard => {
+                designCard.style.display = "inline-block";
+            });
+        }
+
+        function filterMarketing() {
+            hideAllCards();
+
+            var marketingCards = document.querySelectorAll(".marketing");
+            marketingCards.forEach(marketingCard => {
+                marketingCard.style.display = "inline-block";
+            });
+        }
+
+        function hideAllCards() {
+            var allCards = document.querySelectorAll(".card");  
+
+            allCards.forEach(card => {
+                card.style.display = "none";
+            });
+        }
+
+        function showAllCards() {
+            var allCards = document.querySelectorAll(".card");  
+
+            allCards.forEach(card => {
+                card.style.display = "inline-block";
+            });
+        }
