02c4d4ec6b8fe7a94d3eabd3387c05f1aa0092ac
Kristijan Jakimov <kiko.jakimov@gmail.com>
2023-01-28 05:32:16 +0100

    grid problems solved/css modifications added

--- main.js
+++ main.js
@@ -1,47 +1,46 @@
-document.querySelector("#filter-coding").addEventListener("click", filterCoding);
-        document.querySelector("#filter-design").addEventListener("click", filterDesign);
-        document.querySelector("#filter-marketing").addEventListener("click", filterMarketing);
-        document.querySelector("#filter-all").addEventListener("click", showAllCards);
+        document.querySelector("#filter-coding").addEventListener("click", filterCoding);
+         document.querySelector("#filter-design").addEventListener("click", filterDesign);
+         document.querySelector("#filter-marketing").addEventListener("click", filterMarketing);
+         
+         function filterCoding() {
+             hideAllCards();
 
-        function filterCoding() {
-            hideAllCards();
+             var codingCards = document.querySelectorAll(".coding");
+             codingCards.forEach(codingCard => {
+                 codingCard.style.display = "inline-block";
+             });
+         }
 
-            var codingCards = document.querySelectorAll(".coding");
-            codingCards.forEach(codingCard => {
-                codingCard.style.display = "inline-block";
-            });
-        }
+         function filterDesign() {
+             hideAllCards();
 
-        function filterDesign() {
-            hideAllCards();
+             var designCards = document.querySelectorAll(".design");
+             designCards.forEach(designCard => {
+                 designCard.style.display = "inline-block";
+             });
+         }
 
-            var designCards = document.querySelectorAll(".design");
-            designCards.forEach(designCard => {
-                designCard.style.display = "inline-block";
-            });
-        }
+         function filterMarketing() {
+             hideAllCards();
 
-        function filterMarketing() {
-            hideAllCards();
+             var marketingCards = document.querySelectorAll(".marketing");
+             marketingCards.forEach(marketingCard => {
+                 marketingCard.style.display = "inline-block";
+             });
+         }
 
-            var marketingCards = document.querySelectorAll(".marketing");
-            marketingCards.forEach(marketingCard => {
-                marketingCard.style.display = "inline-block";
-            });
-        }
+         function hideAllCards() {
+             var allCards = document.querySelectorAll(".card");  
 
-        function hideAllCards() {
-            var allCards = document.querySelectorAll(".card");  
+             allCards.forEach(card => {
+                 card.style.display = "none";
+             });
+         }
 
-            allCards.forEach(card => {
-                card.style.display = "none";
-            });
-        }
+         function showAllCards() {
+             var allCards = document.querySelectorAll(".card");  
 
-        function showAllCards() {
-            var allCards = document.querySelectorAll(".card");  
-
-            allCards.forEach(card => {
-                card.style.display = "inline-block";
-            });
-        }
+             allCards.forEach(card => {
+                 card.style.display = "inline-block";
+             });
+         }
