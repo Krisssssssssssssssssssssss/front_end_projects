f2db1cd5c31be8fccb57b99e4a34276d530a6610
Kristijan Jakimov <kiko.jakimov@gmail.com>
2023-04-17 01:21:45 +0200

    project finished

--- app.js
+++ app.js
@@ -1,74 +1,131 @@
-const budgetForm = document.querySelector('#budget-form')
-const budgetInput = document.querySelector('.budget-input')
+const budgetForm = $('#budget-form');
+const expenseForm = $('#expense-form');
 
-let amountBudget = document.querySelector('#budget-amount')
-let amountExpense = document.querySelector('#expense-amount')
-let totalBalance = document.querySelector('#balance-amount')
+const budgetInput = $('.budget-input');
+const expenseDescription = $('#expense-input');
+const expenseAmount = $('#amount-input');
 
-const expenseSubmitBtn = document.querySelector('#expense-submit')
+let amountBudget = $('#budget-amount');
+let amountExpense = $('#expense-amount');
+let totalBalance = $('#balance-amount');
 
-const feedbackBudget = document.querySelector('.budget-feedback')
-const feedbackExpense = document.querySelector('.expense-feedback')
+const expenseSubmitBtn = $('#expense-submit');
+
+const feedbackBudget = $('.budget-feedback');
+const feedbackExpense = $('.expense-feedback');
+const table = $('#my-table');
 
 let budget = 0;
 let expenses = 0;
 let balance = budget - expenses;
 
-amountBudget.innerText = budget;
-amountExpense.innerText = expenses;
-totalBalance.innerText = balance;
+amountBudget.text(budget);
+amountExpense.text(expenses);
+totalBalance.text(balance);
 
-// make the input add on to the budget
-// add this to the balance
-budgetForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    // if it is an empty field, throw in an error
-    if (budgetInput.value == '' || budgetInput.value.includes('-')) {
-        feedbackBudget.style.display = "flex";
-    }
-    
+budgetForm.on('submit', function(e) {
+  e.preventDefault();
 
-else {
-    budget += Number(budgetInput.value)
-    balance += Number(budgetInput.value)
-    amountBudget.innerText = budget;
-    totalBalance.innerText = balance;
-    feedbackBudget.style.display = "none";
+  if (budgetInput.val() == '' || budgetInput.val().includes('-')) {
+    feedbackBudget.css('display', 'flex');
+  } else {
+    budget += Number(budgetInput.val());
+    balance += Number(budgetInput.val());
+    amountBudget.text(budget);
+    totalBalance.text(balance);
+  }
+  budgetForm.trigger('reset');
+  budgetInput.on('click', () => {
+    feedbackBudget.css('display', 'none');
+  });
+});
+
+class Expense {
+  constructor(title, value) {
+    this.title = title.val();
+    this.value = value.val();
+  }
 }
-    budgetForm.reset()
-    // on focus on the field again, hide the error
-    // budgetInput.addEventListener('focus', () => {
-    //     feedbackBudget.style.display = "none";
-    // })
-    budgetInput.addEventListener('click', () => {
-        feedbackBudget.style.display = "none";
-    })
-    
 
+expenseForm.on('submit', function(e) {
+  e.preventDefault();
+  if (
+    expenseDescription.val() == '' ||
+    expenseAmount.val() == '' ||
+    expenseAmount.val().includes('-')
+  ) {
+    feedbackExpense.css('display', 'flex');
+  } else {
+    let newExpense = new Expense(expenseDescription, expenseAmount);
+
+    let tr = $('<tr></tr>');
+
+    let tdTitle = $('<td></td>');
+    tdTitle.addClass('text-center');
+    tdTitle.text(newExpense.title);
+
+    let tdValue = $('<td></td>');
+    tdValue.addClass('text-center');
+    tdValue.text(newExpense.value);
+
+    let tdActions = $('<td></td>');
+
+    tr.append(tdTitle, tdValue, tdActions);
+
+    let editBtn = $('<button></button>');
+    editBtn.html(
+      '<i class="fa-solid fa fa-edit" style="color: rgb(125, 163, 221);"></i>'
+    );
+    editBtn.addClass('btn btn-sm m-2');
+    tdActions.append(editBtn);
+
+    let deleteBtn = $('<button></button>');
+    deleteBtn.html('<i class="fa fa-trash" style="color: red;"></i>');
+    deleteBtn.addClass('btn btn-sm m-2');
+    tdActions.append(deleteBtn);
+
+    deleteBtn.on('click', function() {
+      tr.remove();
+
+      expenses -= Number(newExpense.value);
+      balance = budget - expenses;
+      amountExpense.text(expenses);
+      totalBalance.text(balance);
+    });
+
+    editBtn.on('click', function() {
+      expenseDescription.val(newExpense.title);
+      expenseAmount.val(newExpense.value);
+      tr.remove();
+
+      expenses -= Number(newExpense.value);
+      balance = budget - expenses;
+      amountExpense.text(expenses);
+      totalBalance.text(balance);
+    });
+
+    if (table.html() === '') {
+      let thr = $('<tr></tr>');
+      let thdTitle = $('<th></th>');
+      thdTitle.addClass('text-center');
+      thdTitle.text('Expense Title');
+      let thdValue = $('<th></th>');
+      thdValue.addClass('text-center');
+      thdValue.text('Expense Value');
+      let thdActions = $('<th></th>');
+      thdActions.text('');
+      thr.append(thdTitle, thdValue, thdActions);
+      table.append(thr);
+    }
+
+    table.append(tr);
+    expenses += Number(expenseAmount.val());
+balance = budget - expenses;
+amountExpense.text(expenses);
+totalBalance.text(balance);
+ }
+ $('#expense-form').trigger('reset');
+ $('#expense-form').click(() => {
+    $('.expense-feedback').hide();
+ })
 })
-// make expenses add onto the expenses
-// substract this from the balance
-// get the data to create a row
-
-// create a table 
-// create and render heathers if the inner html of the table is empty
-// add a row in the same
-// ...
-
-// if the table has html already just add the new row
-// ...
-
-// ...
-// add all the table datas with all the buttons
-// add event listener to the buttons
-// Delete button:
-// delete the row
-// add the value to the balance
-
-// Edit button:
-// add the table data value to the input
-// change the submit button Text
-// on click, update that row 
-// bring the button text back
-// make expenses add onto the expenses
-// substract this from the balance
